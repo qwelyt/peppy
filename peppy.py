ed82a5d6632b2892da05dd32c549843d1cb6a6ec
qwelyt <qwelyt@users.noreply.github.com>
2022-09-27 23:28:59 +0200

    Playing with shapes

--- peppy.py
+++ peppy.py
@@ -1,5 +1,5 @@
 import cadquery as cq
-from cq_server.ui import ui, show_object, debug
+#from cq_server.ui import ui, show_object, debug
 
 #module chocCut(){
 #  union(){
@@ -16,9 +16,10 @@ def row(thing):
       )
 
 s = 18.2
+c = s-2
 wp = (cq.Workplane('XY')
         .sketch()
-        .rect(s,s)
+        .rect(c,s)
         .rect(13.9,13.9, mode='s')
         .finalize()
         .extrude(3)
@@ -41,32 +42,60 @@ bit = wp +choc
 #show_object(choc)#, options="{'color':(100,100,100)}")
 r = row(wp)
 index = r.translate((-s,-s/4,0))
+long = r
+ring = r.translate((s,-s/2,0))
+pinky = r.translate((s*2,-s,0))
 thumb = (r.rotate((0,0,0),(1,0,0),80)
         .rotate((0,0,0),(0,0,1),-70)
         .translate((-s*2,-s*1.2,-s))
         )
 
+il = cq.Solid.makeLoft([
+    index.faces(">X").val().outerWire(),
+    long.faces("<X").val().outerWire(),
+    ])
+
+lr = cq.Solid.makeLoft([
+    long.faces(">X").val().outerWire(),
+    ring.faces("<X").val().outerWire(),
+    ])
+rp = cq.Solid.makeLoft([
+    ring.faces(">X").val().outerWire(),
+    pinky.faces("<X").val().outerWire(),
+    ])
+
+
+#show_object(index.faces(">YZ"), options={"color":(0,1,0)})#, name="itz")
+#show_object(cq.Workplane().add(il).faces(">YZ"), options={"color":(0,1,0)})
+
+mm = cq.Solid.makeLoft([
+    index.faces(">YZ").val().outerWire(),
+    cq.Workplane().add(il).faces(">YZ").val().outerWire(),
+    ])
+#show_object(mm)
+
+
+
 module = ( index
-        + r
-        + r.translate((s,-s/2,0))
-        + r.translate((s*2,-s,0))
-        + thumb
-        )
-#show_object(module)
-show_object(index)
-show_object(thumb)
-show_object(index.faces("<X"), options={"color":(0,1,0)})
-show_object(thumb.faces("<<Z[5]"), options={"color":(0,1,0)})
-tb = (thumb.faces("<<Z[5]")
-        .workplane(centerOption="CenterOfMass")
-        )
-b = (cq.Workplane().copyWorkplane(tb)
-        .sketch()
-        .rect(s,s)
-        .rect(16,16, mode="s")
-        .finalize()
-        .extrude(20)
-        )#.box(1.5,s,10).translate((0.8,s/2,0))
+        + long
+        + ring
+        + pinky
+#        + thumb
+        +cq.Workplane().add(il).add(lr).add(rp)
+        ).add(mm)
+show_object(module)
+#show_object(cq.Workplane().add(il).add(lr).add(rp))#, options={"color":(1,1,0)})
+#show_object(index)
+#show_object(thumb)
+#show_object(index.faces("<X"), options={"color":(0,1,0)})
+#show_object(thumb.faces("<<Z[5]"), options={"color":(0,1,0)})
+#a = r.translate((s,0,0))
+#b = r.rotate((0,0,0),(0,0,1),6).translate((-s,-s,0))
+#tb = (a.faces("<X").val().outerWire())
+#tc = (b.faces(">X").val().outerWire())
+#td = [tb, tc]
+#show_object(a+b)
+#show_object(cq.Solid.makeLoft(td), options={"color":(0,0,1)})
 #b1 = cq.Workplane().copyWorkplane(tb).box(s,1.5,10).translate((0,0,s/2))
 #show_object(b)
 
